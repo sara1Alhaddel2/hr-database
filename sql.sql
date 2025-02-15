3cdebac29740bb8bb13cb139adc45211c8913964
sara1Alhaddel2 <sarahaddel413@gmail.com>
2025-02-15 20:01:01 +0300

    edit sql.sql file

--- sql.sql
+++ sql.sql
@@ -5,15 +5,3 @@ CREATE TABLE employees (
     email VARCHAR(255) NOT NULL UNIQUE,
     job_title VARCHAR(255) NOT NULL
 );
-
--- Create Jobs Table
-CREATE TABLE jobs (
-    job_id SERIAL PRIMARY KEY,
-    job_title VARCHAR(255) NOT NULL
-);
-
--- Create Departments Table
-CREATE TABLE departments (
-    department_id SERIAL PRIMARY KEY,
-    department_name VARCHAR(255) NOT NULL
-);
